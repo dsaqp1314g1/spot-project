08ee5315825a9b4414defa65bce1be6550de09a2
anguela <albert.anguela.calvet@gmail.com>
2014-06-09 14:14:41 +0200

    aver si os funciona ahora cabrones

--- www/main.js
+++ www/main.js
@@ -88,35 +88,17 @@ function initialize(myLatlng, contentString, idspot) {
 
 $("#button-list-spots").click(function(e) {
 	e.preventDefault();
-	if ($('#buscar_ciud').val() === '')
-	{
-	$('#exam-error').show();	
-	//getSpotsParam($("#buscar_ciud").val(),$("#buscar_mod").val());
-	}
-else
-{
 	getSpotsParam($("#buscar_ciud").val(),$("#buscar_mod").val());
-}
 });
 $('#buscar-amigo').click(function(e) {
 	e.preventDefault();	
 	$('#comment-form').hide();
 	//getUserParam($("#buscar_campo").val());
 });
-
 $("#button-list-one").click(function(e) {
 	e.preventDefault();
-	if ($('#buscar_id').val() === '')
-	{
-	$('#exam-error').show();	
-	//getSpotsParam($("#buscar_ciud").val(),$("#buscar_mod").val());
-	}
-else
-{
 	getSpotId($("#buscar_id").val());
-}
 });
-
 $('#button-delete-comment').click(function(e) {
 	e.preventDefault();					
 	deleteComment($("#buscar_ciud").val());
@@ -125,17 +107,10 @@ $('#comment-cancel').click(function(e) {
 	e.preventDefault();					
 	$("#edit-comment").val('');
 });
-
-$("#closing").click(function() {
-	$("#exam-error").hide();
-});
-
 function getSpots() {
 	var url = API_BASE_URL + '/spots';
 	$('progress').toggle();
-
 	$("#repos_result").text("");
-	
 	
 	$.ajax({
 		url : url,
@@ -148,37 +123,13 @@ function getSpots() {
 
 				$.each(repos.spots, function(i, v) {
 					var spot = new Spot(v);
-
-					console.log(JSON.stringify(spot));
-					var a = parseFloat(spot.latitud);
-					var b = parseFloat(spot.longitud);
-					var c = spot.longitud +","+spot.latitude;
-					//$('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': c, 'bounds': true } );
-					$('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '22.345573,-22.098326', 'bounds': true } );
-					$('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': new google.maps.LatLng(spot.latitud, spot.longitud), 'bounds': true } );
-
-					$('<h4> ID: ' + spot.idspot + '</h4>').appendTo($('#repos_result'));				
-					$('<strong> Usuario: </strong> ' + spot.usuario + '<br>').appendTo($('#repos_result'));
-					$('<strong> Ciudad: </strong> ' + spot.ciudad + '<br>').appendTo($('#repos_result'));
-					$('<strong> Deporte: </strong> ' + spot.deporte + '<br>').appendTo($('#repos_result'));
-					var link = $('<a id="sting-link" href="'+spot.getLinks("abrir-spot").href+'">'+"Spot detail" +'</a>');
-					link.click(function(e){
-						e.preventDefault();
-						idspot = spot.idspot;
-						loadSpot($(e.target).attr('href'));
-						return false;
-					});
-					var div = $('<div></div>')
-					div.append(link);
-					$('#repos_result').append(div);
-
 					var idmarker = spot.idspot;
 					var contentString ='<h4> ID: ' + spot.idspot + '</h4>'+ 
 					'<strong> Usuario: </strong> ' + spot.usuario + '<br>'+
 					'<strong> Ciudad: </strong> ' + spot.ciudad + '<br>'+
 					'<strong> Deporte: </strong> ' + spot.deporte + '<br>';
 					var myLatlng = new google.maps.LatLng(spot.latitud, spot.longitud);
-					initialize(myLatlng, contentString, idmarker);
+					initialize(myLatlng, contentString, idmarker);								
 				});												
 
 	}).fail(function() {
@@ -213,21 +164,14 @@ function getSpotsParam(ciudad, modal) {
 				$('progress').toggle();
 				$.each(repos.spots, function(i, v) {
 					var spot = new Spot(v);
-					$('<h4> ID: ' + spot.idspot + '</h4>').appendTo($('#repos_result'));				
-					$('<strong> Usuario: </strong> ' + spot.usuario + '<br>').appendTo($('#repos_result'));
-					$('<strong> Ciudad: </strong> ' + spot.ciudad + '<br>').appendTo($('#repos_result'));
-					$('<strong> Deporte: </strong> ' + spot.deporte + '<br>').appendTo($('#repos_result'));
-					var link = $('<a id="sting-link" href="'+spot.getLinks("abrir-spot").href+'">'+"Spot detail" +'</a>');
-					link.click(function(e){
-						e.preventDefault();
-						loadSpot($(e.target).attr('href'));
-						return false;
-					});
-					var div = $('<div></div>')
-					div.append(link);
-					$('#repos_result').append(div);
-				});
-				
+					var idmarker = spot.idspot;
+					var contentString ='<h4> ID: ' + spot.idspot + '</h4>'+ 
+					'<strong> Usuario: </strong> ' + spot.usuario + '<br>'+
+					'<strong> Ciudad: </strong> ' + spot.ciudad + '<br>'+
+					'<strong> Deporte: </strong> ' + spot.deporte + '<br>';
+					var myLatlng = new google.maps.LatLng(spot.latitud, spot.longitud);
+					initialize(myLatlng, contentString, idmarker);								
+				});		
 
 	}).fail(function() {
 		$('progress').toggle();
@@ -270,6 +214,4 @@ $(document).ready(function(){
 // }); var mapOptions = {
 	// this works! (lat, lng are global variables read from localStorage
 	getSpots();
-	$('#map_canvas').gmap({'center': '-34.397, 100.644'}).bind('init', function() { 
-		   $('#map_canvas').gmap('option', 'zoom', 2); });
 });

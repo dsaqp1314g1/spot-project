92e8e6c752a3d78df57196e4144a6b1bdae8f62d
anguela <albert.anguela.calvet@gmail.com>
2014-06-12 13:08:06 +0200

    android search by

--- spot-android/src/edu/upc/eetac/dsa/dsaqp1314g1/spot/android/SpotSearchActivity.java
+++ spot-android/src/edu/upc/eetac/dsa/dsaqp1314g1/spot/android/SpotSearchActivity.java
@@ -18,7 +18,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import edu.upc.eetac.dsa.dsaqp1314g1.spot.android.api.Spot;
 import edu.upc.eetac.dsa.dsaqp1314g1.spot.android.api.SpotCollection;
 import edu.upc.eetac.dsa.dsaqp1314g1.spot.android.api.SpotgramAPI;
@@ -27,6 +29,8 @@ import edu.upc.eetac.dsa.dsaqp1314g1.spot.android.api.SpotgramAndroidException;
 public class SpotSearchActivity extends ListActivity {
 	String serverAddress;
 	String serverPort;
+	EditText nombrecity;
+	EditText nombresport;
 	private class FetchSpotsTask extends
 			AsyncTask<Void, Void, SpotCollection> {
 		private ProgressDialog pd;
@@ -62,6 +66,7 @@ public class SpotSearchActivity extends ListActivity {
 
 	}
 	private void addSpots(SpotCollection Spots){
+		SpotList.clear();
 		SpotList.addAll(Spots.getSpots());
 		adapter.notifyDataSetChanged();
 	}
@@ -73,7 +78,10 @@ public class SpotSearchActivity extends ListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.beeter_layout);
+		setContentView(R.layout.beeter_layout);	
+
+		nombrecity = (EditText) findViewById(R.id.textCity);
+		nombresport = (EditText) findViewById(R.id.textSport);	
 		SharedPreferences prefs = getSharedPreferences("spot-profile",
 				Context.MODE_PRIVATE);
 		final String username = prefs.getString("username", null);
@@ -104,7 +112,33 @@ public class SpotSearchActivity extends ListActivity {
 		(new FetchSpotsTask()).execute();
 	}
 	
-
+	public void searchAll(View v) {
+	
+	}
+    public void searchBy(View v) {
+    	AssetManager assetManager = getAssets();
+    	Properties config = new Properties();
+    	try {
+    		config.load(assetManager.open("config.properties"));
+    		serverAddress = config.getProperty("server.address");
+    		serverPort = config.getProperty("server.port");
+     
+    		Log.d(TAG, "Configured server " + serverAddress + ":" + serverPort);
+    	} catch (IOException e) {
+    		Log.e(TAG, e.getMessage(), e);
+    		finish();
+    	}
+    	String ciudad = nombrecity.toString();
+    	String modal = nombresport.toString();
+    	URL url = null;
+    	try {
+    		url = new URL("http://" + serverAddress + ":" + serverPort
+    				+ "/spot-api/spots/search?ciudad="+ciudad+"&deporte="+modal);
+    	} catch (MalformedURLException e) {
+    		return;
+    	}
+		(new FetchSpotingTask()).execute(url);
+    }
 	@Override
 	// detecta click en la pantalla
 	protected void onListItemClick(ListView l, View v, int position, long id) {
@@ -131,6 +165,39 @@ public class SpotSearchActivity extends ListActivity {
 //		intent.putExtra("url", Spot.getLinks().get("abrir-spot").getTarget());
 //		startActivity(intent);
 	}
+	private class FetchSpotingTask extends AsyncTask<URL, Void, SpotCollection> {
+		private ProgressDialog pd;
 
+		@Override
+		protected SpotCollection doInBackground(URL... params) {
+			SpotCollection Spots = null;
+			try {
+				Spots = SpotgramAPI.getInstance(SpotSearchActivity.this)
+						.getSpots();
+			} catch (SpotgramAndroidException e) {
+				e.printStackTrace();
+			}
+			return Spots;
+		}
+
+		@Override
+		protected void onPostExecute(SpotCollection result) {
+			addSpots(result);
+			if (pd != null) {
+				pd.dismiss();
+			}
+		}
+
+		@Override
+		protected void onPreExecute() {
+			pd = new ProgressDialog(SpotSearchActivity.this);
+			pd.setTitle("Searching...");
+			pd.setCancelable(false);
+			pd.setIndeterminate(true);
+			pd.show();
+		}
+
+	 
+	}
 
 }
